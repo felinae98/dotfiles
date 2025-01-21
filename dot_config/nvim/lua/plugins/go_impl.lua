2731191f034161bc869692a194f8dbcca6217580
felinae98 <731499577@qq.com>
2025-01-21 17:32:33 +0800

    Add .config/nvim/lua/plugins/go_impl.lua

---
+++ dot_config/nvim/lua/plugins/go_impl.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+return {
+  "edolphin-ydf/goimpl.nvim",
+  lazy = true,
+  specs = {
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = {
+        { "nvim-lua/plenary.nvim" },
+        { "nvim-lua/popup.nvim" },
+        { "nvim-treesitter/nvim-treesitter" },
+      },
+      opts = function() require("telescope").load_extension "goimpl" end,
+    },
+  },
+}
