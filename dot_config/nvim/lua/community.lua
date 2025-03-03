b7c501cb4b2505e078bef38be31dc33003e08564
felinae98 <731499577@qq.com>
2025-03-03 19:53:33 +0800

    Update .config/nvim/lua/community.lua

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -40,6 +40,21 @@ return {
 	--     },
 	--   },
 	-- },
+	{ import = "astrocommunity.completion.avante-nvim" },
+	{
+		"yetone/avante.nvim",
+		opts = {
+			provider = "deepseek",
+			vendors = {
+				deepseek = {
+					__inherited_from = "openai",
+					api_key_name = "DEEPSEEK_API_KEY",
+					endpoint = "https://api.deepseek.com",
+					model = "deepseek-chat",
+				},
+			},
+		},
+	},
 	{ import = "astrocommunity.editing-support.suda-vim" },
 	{ import = "astrocommunity.git.diffview-nvim" },
 	{ import = "astrocommunity.git.neogit" },
