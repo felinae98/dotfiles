5a381726105898a1a73baf2476ca56d1993c4a72
felinae98 <731499577@qq.com>
2025-03-12 23:38:28 +0800

    Update .config/nvim/lua/community.lua

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -27,34 +27,22 @@ return {
 		},
 	},
 	-- { import = "astrocommunity.completion.codeium-nvim" },
-	-- { import = "astrocommunity.completion.copilot-lua" },
-	-- {
-	--   "zbirenbaum/copilot.lua",
-	--   opts = {
-	--     suggestion = {
-	--       keymap = {
-	--         accept = "<C-H>",
-	--         prev = "<C-,>",
-	--         next = "<C-.>",
-	--       },
-	--     },
-	--   },
-	-- },
+	{ import = "astrocommunity.completion.copilot-lua" },
 	{ import = "astrocommunity.completion.avante-nvim" },
-	{
-		"yetone/avante.nvim",
-		opts = {
-			provider = "deepseek",
-			vendors = {
-				deepseek = {
-					__inherited_from = "openai",
-					api_key_name = "DEEPSEEK_API_KEY",
-					endpoint = "https://api.deepseek.com",
-					model = "deepseek-chat",
-				},
-			},
-		},
-	},
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	opts = {
+	-- 		provider = "deepseek",
+	-- 		vendors = {
+	-- 			deepseek = {
+	-- 				__inherited_from = "openai",
+	-- 				api_key_name = "DEEPSEEK_API_KEY",
+	-- 				endpoint = "https://api.deepseek.com",
+	-- 				model = "deepseek-chat",
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 	{ import = "astrocommunity.editing-support.suda-vim" },
 	{ import = "astrocommunity.git.diffview-nvim" },
 	{ import = "astrocommunity.git.neogit" },
