0153be015e04fe14d5b7c3619e3b8f601b36b45e
felinae98 <731499577@qq.com>
2025-10-15 15:51:06 +0800

    update

--- .chezmoiscripts/run_onchange_after_030-setup-sshkey.sh
+++ .chezmoiscripts/run_onchange_after_030-setup-sshkey.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mkdir -p ~/.ssh
+touch ~/.ssh/known_hosts
+
 if ! ssh-keygen -F '[ssh.github.com]:443'; then
-  ssh-keyscan -p 443 -t rsa,ecdsa,ed25519 ssh.github.com >> ~/.ssh/known_hosts
+  ssh-keyscan -p 443 -t rsa,ecdsa,ed25519 ssh.github.com >>~/.ssh/known_hosts
 fi
